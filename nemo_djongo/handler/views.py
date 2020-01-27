62d06a442ab839e594a726020aa10d0fc309132c
rcrdoh <rcrdobregon@gmail.com>
2020-01-27 18:35:03 -0500

    depurando

--- nemo_djongo/handler/views.py
+++ nemo_djongo/handler/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-#from handler.models import Nodos
+
 from . import models
 import json
 import socketio
